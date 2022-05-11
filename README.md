d85129631cfd62bf8515ecfe0baef4d5ecff7500
unicate <raoulschmid@bluewin.ch>
2022-05-11 17:34:21 +0200

    Init

---
+++ README.md
@@ -0,0 +1,42 @@
+# Static HTML Bulma Website
+
+## What's included?
+
+- [Bulma](https://bulma.io/): Is a modern CSS framework that just works.
+- [Fontawesome-free](https://fontawesome.com/icons?m=free): Icons are ready to use.
+- [Cash-Dom](https://github.com/fabiospampinato/cash): Small jQuery alternative.
+- [html-includes](https://www.npmjs.com/package/html-includes): HTML compilation with partial includes.
+
+## Install
+
+```
+npm install
+```
+
+## Build
+
+### Development
+
+Build for DEV and watch for changes.
+```
+npm run dev
+```
+or just local DEV build
+```
+npm run build
+```
+
+### Production
+
+Minified build for PROD.
+
+```
+npm run build-prod
+```
+
+#
+
+
+## Disclaimer & License
+
+This project is released under the [MIT](https://raw.githubusercontent.com/unicate/licenses/master/MIT/MIT-Licence.txt) licence.
---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+const $ = require('cash-dom');
+const Cookies = require('js-cookie');
+const COOKIE_NAME = 'cookie-consent';
+
+
+$(document).ready(function () {
+    /**
+     * Navbar Burger
+     */
+    $(".navbar-burger").on('click', function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+
+    /**
+     * Navbar mark active
+     */
+    $('a.navbar-item').removeClass('is-active');
+
+    if (location.href.endsWith('disclaimer.html')){
+        $('a.navbar-item:nth-child(2)').addClass('is-active');
+    } else {
+        $('a.navbar-item:nth-child(1)').addClass('is-active');
+    }
+
+    /**
+     * Cookie handling for cookiebar.
+     */
+    let cookieVal = Cookies.get(COOKIE_NAME);
+    if (cookieVal != 'YES') {
+        $('#cookiebar').removeClass('is-hidden');
+    }
+    $('#btn-cookiebar-ok').on('click', function () {
+        Cookies.set(COOKIE_NAME, 'YES');
+        $('#cookiebar').addClass('is-hidden');
+    });
+    $('#btn-cookiebar-delete').on('click', function () {
+        $('#cookiebar').addClass('is-hidden');
+    });
+});
